b006f5ffe4f7b43da97937c69a8ab4ce96a43fc0
Menukakarki <menukakarki365@gmail.com>
2025-05-29 15:07:17 +0545

    Remove app.run and update index.html for Render deployment

--- app.py
+++ app.py
@@ -97,5 +97,5 @@ def delete(sno):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=8000)
+    # app.run(debug=True, port=8000)
 # this is app.py
